ddf58e2feffcf9ca9cecd60f799ef88f25fbda20
Maxwell Baird <bairdm71@gmail.com>
2020-01-30 13:39:15 -0700

    added test for max_age and then add the method for max_age to event.rb

--- lib/event.rb
+++ lib/event.rb
@@ -5,4 +5,18 @@ class Event
     @name = event_name
     @ages = ages_for_event
   end
+
+  def max_age
+    max_age_holder = 0
+    @ages.each do |age|
+
+      if age > max_age_holder
+        max_age_holder = age
+      end
+
+    end
+    max_age_holder
+  end
+
+
 end
--- test/event_test.rb
+++ test/event_test.rb
@@ -8,4 +8,24 @@ class EventTest < Minitest::Test
     event = Event.new("Curling", [24, 30, 18, 20, 41])
     assert_instance_of Event, event
   end
+
+  def test_if_event_has_name
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal "Curling", event.name
+  end
+
+  def test_if_event_has_ages
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal [24, 30, 18, 20, 41], event.ages
+  end
+
+  def test_event_max_age
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 41, event.max_age
+  end
+
+  def test_event_min_age
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 18, event.max_age
+  end
 end
